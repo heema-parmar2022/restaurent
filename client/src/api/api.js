5867660c9a8993ea93f746434a839a84d330efd4
Heema Parmar <heema.parmar@thegatewaycorp.co.in>
2026-04-13 15:00:53 +0530

    Add production api uurl from render server for node

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "production"
+    ? "https://restaurent-tiz5.onrender.com/api"
+    : "http://localhost:5000/api";
+
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL,
 });
